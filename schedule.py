d43cf93f498dc044639843d878c762aa8206713d
jolesinski <olesinski.jakub@gmail.com>
2013-12-05 11:39:05 +0100

    Schedule class splitted into two files, added scheduling main module

---
+++ schedule.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 10:48:50 2013
+
+@author: Jakub
+"""
+
+from  scheduler.Scheduler import Scheduler
+
+def main():
+    sched = Scheduler(10)
+
+
+if __name__ == '__main__':
+    main()
